031655bcc988188d90a745d4d8e2c41574f3b190
wilson <rajwii44@gmail.com>
2025-07-01 15:16:47 -0300

    criado o metodo de hash de senha usando bcrypt

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -4,36 +4,42 @@ import { Entity } from "../shared/entity";
 //Formato de entrada dos dados
 export type UserCreateDto = {
     email: string;
-    password : string;
+    password: string;
 }
 
-export class User extends Entity{
+export class User extends Entity {
     private constructor(
-        id : string,
-        private email : string,
-        private password : string,
-        updateAt : Date,
-        createAt : Date
-    ){
-        super(id,createAt,updateAt);
+        id: string,
+        private email: string,
+        private password: string,
+        updateAt: Date,
+        createAt: Date
+    ) {
+        super(id, createAt, updateAt);
         this.validate();
     }
 
-    public static create({email , password}: UserCreateDto):User {
+    public static create({ email, password }: UserCreateDto): User {
         const id = Utils.GenerateUUID();
+        const hasedPassword = Utils.HashedPassword(password); // chamo a funcao de criar hash de senhas
         const createAt = new Date();
         const updateAt = new Date();
 
-        return new User(id ,email,password, createAt , updateAt);
+        return new User(id, email, hasedPassword, createAt, updateAt);
     }
 
-    protected validate(): void {};
+    protected validate(): void { };
 
-    public getEmail(): string{
+    public getEmail(): string {
         return this.email;
     }
-    public getPassword(): string{
+    public getPassword(): string {
         return this.password;
     }
     
+    // chamo a funcao de verificar a senha pasando a senha atual
+    public comparePassword(aPassword : string): boolean{
+        return Utils.ComparePassword(aPassword , this.password);
+    }
+
 }
--- src/shared/utils/utils.ts
+++ src/shared/utils/utils.ts
@@ -1,5 +1,25 @@
+import * as bcrypt from 'bcrypt';
+
 export class Utils{
     public static GenerateUUID(): string{
         return crypto.randomUUID()
+    }
+
+    //Gerar a senha com hash
+    public static HashedPassword(password : string ) : string{
+        const salt = this.GenSalt();
+        const passwordHashed = bcrypt.hashSync(password , salt);
+        return passwordHashed;
+    }
+
+    //Verificar se a senha pasada confere com a salva
+    public static ComparePassword( password : string , hasedPassword): boolean{
+        return bcrypt.compareSync(password , hasedPassword )
+    }
+    // Gerado de carecteres para o hash da senha
+    private static GenSalt(): string{
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        return salt;
     }
 }
